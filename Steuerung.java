d9ea67c180086d8eae72698d8c5b8e0dc1f245b3
Buraksen67 <132590165+Buraksen67@users.noreply.github.com>
2024-03-10 20:59:56 +0100

    Add files via upload

---
+++ Steuerung.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * Description
+ *
+ * @version 1.0 from 10.03.2024
+ * @author 
+ */
+
+public class Steuerung {
+  
+  // start attributes
+  private GUI dieGUI;
+  private Fragen dieFragen;
+  // end attributes
+  
+  public Steuerung(GUI pGUI) {
+    this.dieGUI = null;
+    this.dieFragen = null;
+  }
+
+  // start methods
+  // end methods
+} // end of Steuerung
