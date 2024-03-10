771a25bd84673f25f7d669088db4cd02e19e6a2a
Buraksen67 <132590165+Buraksen67@users.noreply.github.com>
2024-03-10 20:56:15 +0100

    Add files via upload

---
+++ Java-Programm/Fragen.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * Description
+ *
+ * @version 1.0 from 10.03.2024
+ * @author 
+ */
+
+public class Fragen {
+  
+  // start attributes
+  // end attributes
+  
+  public Fragen() {
+  }
+
+  // start methods
+  // end methods
+} // end of Fragen
