52595e69f934a415102c53e965b151f4ed6446c8
HayashiWRLD <162364709+HayashiWRLD@users.noreply.github.com>
2024-03-28 20:29:30 +0100

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -3,44 +3,52 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-/**
- *
- * Description
- *
- * @version 1.0 from 10.03.2024
- * @author 
- */
-
 public class GUI extends JFrame {
   // Assoziationen
+  // Anfang Attribute
   Steuerung DieST;
   // end attributes
   
   //Components
-  private JLabel questionLabeL;
+  private JLabel questionLabel;
   private JButton[] answerButtons;
+  //private JLabel[] priceLabel;
+  private JLabel[] priceLabel;
   private JButton quitButton;
-  private int currentQuestionIndex;
+  private JLabel timerLabel;
+  private int currentQuestionIndex = 0;
+  private Timer timer;
+  private int secondsLeft = 15;
   
-  // Questions and answers (simplified)
-  private String[] questions = {
-          "What is the capital of France?",
-          "Which planet is known as the Red Planet?",
-          "What is the largest mammal on Earth?"
-    };
-  
-  private String[][] answers = {
-          {"Berlin", "Madrid", "Paris", "Rome" },
-          {"Venus", "Mars", "Jupiter", "Saturn"},
-          {"Elephant", "Blue Whale", "Giraffe", "Lion"}
-    };
-  
-  public GUI() { 
+  public String Questions[] = {
+    "What is the capital of France?",
+    "Which planet is called the red planet?",
+    "What animal is the largest one on earth?",
+    "Which of the following historical figures had the greatest influence on modern physics?"
+  };
+  public String Answers[][] = {
+    {"Rome", "Madrid", "Paris", "Berlin"},
+    {"Venus", "Mars", "Jupiter", "Earth"},
+    {"Elephant", "Blue Whale", "Lion", "Rhino"},
+    {"Isaac Newton", "Albert Einstein", "Galileo Galilei", "Marie Curie"}
+  };
+  public int CorrectAnswers[] = {
+    2, 1, 1, 1
+  };
+  private String priceText[] = {
+  "50€",
+  "100€",
+  "200€",
+  "300€"
+  };
+
+  // Ende Attribute
+  public GUI() {
     // Frame init
     super();
     setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    int frameWidth = 500;
-    int frameHeight = 300;
+    int frameWidth = 900;
+    int frameHeight = 400;
     setSize(frameWidth, frameHeight);
     Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
     int x = (d.width - getSize().width) / 2;
@@ -51,69 +59,145 @@ public class GUI extends JFrame {
     Container cp = getContentPane();
     cp.setLayout(null);
     // start components
-    questionLabeL = new JLabel();
-    questionLabeL.setBounds(50,20,400,30);
-    questionLabeL.setHorizontalAlignment(JLabel.CENTER);
+    questionLabel = new JLabel();
+    questionLabel.setBounds(15, 30, 475, 30);
+    questionLabel.setHorizontalAlignment(JLabel.CENTER);
     
     answerButtons = new JButton[4];
     for (int i = 0; i < 4; i++) {
       answerButtons[i] = new JButton();
-      answerButtons[i].setBounds(50, 70 + i * 40, 400, 30);
+      answerButtons[i].setBounds(50, 100 + i * 40, 700, 30);
       answerButtons[i].addActionListener(new AnswerButtonListener());
       cp.add(answerButtons[i]);
     } // end of for
     
+    priceLabel = new JLabel[4];
+    questionLabel.setHorizontalAlignment(JLabel.RIGHT);
+    for (int i = 0; i < 4 ; i++ ) {
+      priceLabel[i] = new JLabel();
+      priceLabel[i].setBounds(800, 70 - 20 * i, 100, 300);
+      priceLabel[i].setText(priceText[i]);
+      cp.add(priceLabel[i]);
+      setVisible(false);
+    } // end of for
+    
+    
     quitButton = new JButton("Quit");
-    quitButton.setBounds(200, 230, 100, 30);
-    quitButton.addActionListener(new ActionListener(){;
+    quitButton.setBounds(350, 300, 100, 30);
+    quitButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(GUI.this, "Thanks for playing!");
         System.exit(0);
       }
     });
     
-    cp.add(questionLabeL);
+    timerLabel = new JLabel();
+    timerLabel.setBounds(10, 10, 80, 20);
+    timerLabel.setHorizontalAlignment(JLabel.RIGHT);
+    timerLabel.setText("Time: " + secondsLeft);
+    
+    cp.add(questionLabel);
     cp.add(quitButton);
+    cp.add(timerLabel);
     
     setVisible(true);
     DieST = new Steuerung(this);
     
     // Start the game with the first question
     nextQuestion();
-  } // end of public GUI
     
+    // Start the timer
+    timer = new Timer(1000, new TimerListener());
+    timer.start();
+  } // end of public GUI
+    // Anfang Komponenten
+    // Ende Komponenten
+  // Anfang Methoden
+
     // start methods
   private void nextQuestion() {
-    if (currentQuestionIndex < questions.length) {
-      questionLabeL.setText(questions[currentQuestionIndex]);
+    if (currentQuestionIndex < Questions.length) {
+      questionLabel.setText(Questions[currentQuestionIndex]);
       
       for (int i = 0; i < 4; i++) {
-        answerButtons[i].setText(answers[currentQuestionIndex][i]);
+        answerButtons[i].setText(Answers[currentQuestionIndex][i]); //answers[currentQuestionIndex][i]);
       } // end of for
-      currentQuestionIndex++;
+      
+      for (int i = 0; i < 4 ; i++) {
+        if (i== currentQuestionIndex) {
+          priceLabel[i].setFont(new Font("Courier", Font.BOLD, 25));
+        } // end of if
+        else {
+          priceLabel[i].setFont(new Font("Courier", Font.BOLD, 15));
+        } // end of if-else
+      } // end of for
     } else {
       JOptionPane.showMessageDialog(this, "Congratulations! You've answered all questions. ");
       System.exit(0);
-    } // end of if-else 
+    } // end of if-else
   }
-  
+
   private class AnswerButtonListener implements ActionListener {
+  // Anfang Attribute1
+  // Ende Attribute1
+  // Anfang Methoden1
+  
+    public void OnRightAnswer() {
+      timer.stop();
+      JOptionPane.showMessageDialog(GUI.this, "You are right!");
+      currentQuestionIndex++;
+      secondsLeft = 15; // Reset the timer
+      timer.start();
+    };
+    
+    public void OnWrongAnswer() {
+      timer.stop();
+      JOptionPane.showMessageDialog(GUI.this, "You are wrong!");
+    };
+    
     public void actionPerformed(ActionEvent e) {
       JButton sourceButton = (JButton) e.getSource();
-      String selectedAnswer = sourceButton.getText();
-      JOptionPane.showMessageDialog(GUI.this, "Your answer: " + selectedAnswer);
+      if(sourceButton.getText() == Answers[currentQuestionIndex][CorrectAnswers[currentQuestionIndex]])
+      {
+        OnRightAnswer();
+      }
+      else 
+      {
+        OnWrongAnswer();  
+      } // end of if-else
       
       // Move to the next question
       nextQuestion();
     }
-  }  
-  
+    // Anfang Komponenten1
+    // Ende Komponenten1
+  // Ende Methoden1
+  }
+
+  private class TimerListener implements ActionListener {
+  // Anfang Attribute2
+  // Ende Attribute2
+  // Anfang Methoden2
+    public void actionPerformed(ActionEvent e) {
+      secondsLeft--;
+      timerLabel.setText("Time: " + secondsLeft);
+      if (secondsLeft == 0) {
+        timer.stop();
+        JOptionPane.showMessageDialog(GUI.this, "Time's up!");
+      }
+    }
+    // Anfang Komponenten2
+    // Ende Komponenten2
+  // Ende Methoden2
+  }
+
   public static void main(String[] args) {
-    SwingUtilities.invokeLater(new Runnable() {;
+    SwingUtilities.invokeLater(new Runnable() {
       public void run() {
         new GUI();
       }
     });
   } // end of main
+  // Ende Methoden
     // end methods
-  } // end of class GUI
+} // end of class GUI
